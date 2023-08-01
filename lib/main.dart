3af2a01fc727f2109bfa1e0f8fb13d961259286f
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-08-01 11:20:36 +0330

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatefulWidget {
+  const Application({super.key});
+
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        body: SafeArea(child: Text("Ehsan Salehi")),
+      ),
+    );
+  }
+}
